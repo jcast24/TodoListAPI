cda35bb34268229403c61a8afe0db3106f8206d5
Jesus Castro <castrojesus39@gmail.com>
2025-06-13 17:22:18 -0700

    registered DBcontext via DI

--- Program.cs
+++ Program.cs
@@ -1,3 +1,6 @@
+using Microsoft.EntityFrameworkCore;
+using TodoApi.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,12 +9,27 @@ builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
 
+/*
+    Services such as DB context must be registered with the dependency injection (DI) container. The container 
+    provides services to controller.
+
+    Specifies that the database context will use an in-memory database.
+*/
+builder.Services.AddDbContext<TodoContext>(opt =>
+{
+    opt.UseInMemoryDatabase("TodoList");
+});
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
+    app.UseSwaggerUi(options =>
+    {
+        options.DocumentPath = "/openapi/v1.json";
+    });
 }
 
 app.UseHttpsRedirection();
