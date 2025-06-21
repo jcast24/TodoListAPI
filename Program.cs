e91d6ed032d5be4f23a3ffedb6b648370ebd8c69
Jesus Castro <castrojesus39@gmail.com>
2025-06-20 23:12:21 -0700

    implemented the TodoItemRepo + interface and TodoItemService + interface into services via DI

--- Program.cs
+++ Program.cs
@@ -1,5 +1,7 @@
 using Microsoft.EntityFrameworkCore;
 using TodoApi.Models;
+using TodoApi.Repository;
+using TodoApi.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -8,6 +10,7 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
+builder.Services.AddEndpointsApiExplorer();
 
 /*
     Services such as DB context must be registered with the dependency injection (DI) container. The container 
@@ -19,6 +22,10 @@ builder.Services.AddDbContext<TodoContext>(opt =>
 {
     opt.UseInMemoryDatabase("TodoList");
 });
+
+// DI
+builder.Services.AddScoped<ITodoItemRepository, TodoItemRepository>();
+builder.Services.AddScoped<ITodoItemService, TodoItemService>();
 
 var app = builder.Build();
 
@@ -32,7 +39,7 @@ if (app.Environment.IsDevelopment())
     });
 }
 
-app.UseHttpsRedirection();
+//app.UseHttpsRedirection();
 
 app.UseAuthorization();
 
