8fe98e368e75a5cd72b1a67bf4baafc8b176a28b
Jesus Castro <castrojesus39@gmail.com>
2025-06-20 23:09:51 -0700

    Updated controller file to implement service files for each HTTP operation

--- Controllers/TodoItemsController.cs
+++ Controllers/TodoItemsController.cs
@@ -1,11 +1,9 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using TodoApi.Services;
 using TodoApi.Models;
+using System.Runtime.CompilerServices;
+using NuGet.Protocol.Core.Types;
 
 namespace TodoApi.Controllers
 {
@@ -18,121 +16,47 @@ namespace TodoApi.Controllers
     public class TodoItemsController : ControllerBase
     {
 
-        // Line 22-27, DI implementation
-        private readonly TodoContext _context;
+        private readonly ITodoItemService _todoService;
 
-        public TodoItemsController(TodoContext context)
+        public TodoItemsController(ITodoItemService todoService)
         {
-            _context = context;
+            _todoService = todoService;
         }
 
-        // GET: /api/TodoItems
-        // returns all the items in TodoItems as a list. 
         [HttpGet]
-        public async Task<ActionResult<IEnumerable<TodoItemDTO>>> GetTodoItems()
+        public async Task<ActionResult<IEnumerable<TodoItem>>> GetTodoItems()
         {
-            return await _context.TodoItems.Select(x => ItemToDTO(x)).ToListAsync();
+            var todo = await _todoService.GetAllTodoItems();
+            return Ok(todo);
         }
 
-        // GET: /api/TodoItems/5
-        // returns a single item from TodoItems based on an id. 
         [HttpGet("{id}")]
-        public async Task<ActionResult<TodoItemDTO>> GetTodoItem(long id)
+        public async Task<ActionResult> GetTodoItem(int id)
         {
-            var todoItem = await _context.TodoItems.FindAsync(id);
+            var todo = await _todoService.GetTodoItemAsync(id);
 
-            // if there aren't any items, return NotFound()
-            if (todoItem == null)
+            if (todo == null)
             {
                 return NotFound();
             }
-
-            // return the item if found
-            return ItemToDTO(todoItem);
+            return Ok(todo);
         }
 
-        // PUT: api/TodoItems/5
-        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
-        [HttpPut("{id}")]
-        public async Task<IActionResult> PutTodoItem(long id, TodoItemDTO todoDTO)
-        {
-            // if the id doesn't equal to the id in TodoItem
-            // return BadRequest()
-            if (id != todoDTO.Id)
-            {
-                return BadRequest();
-            }
-
-            // _context.Entry(todoDTO).State = EntityState.Modified;
-
-            var todoItem = await _context.TodoItems.FindAsync(id);
-            if (todoItem == null)
-            {
-                return NotFound();
-            }
-
-            todoItem.Name = todoDTO.Name;
-            todoItem.IsComplete = todoDTO.IsComplete;
-
-            try
-            {
-                await _context.SaveChangesAsync();
-            }
-            catch (DbUpdateConcurrencyException) when (!TodoItemExists(id))
-            {
-                return NotFound();
-            }
-
-            return NoContent();
-        }
-
-        // POST: api/TodoItems
-        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
-        /*
-            CREATE
-            This method gets the value of the TodoItem from the body of the HTTP request.
-            The CreatedAtAction method: 
-                - returns an HTTP 201 status code. Standard response for an HTTP POST method that creates a new resource on the server
-                - Adds a location header to the response. 
-                - References the GetItemTodo action to create the Location header's URI. The nameof keyword is used to avoid hard-coding
-                the action name in the CreatedAtAction call. 
-        */
         [HttpPost]
-        public async Task<ActionResult<TodoItemDTO>> PostTodoItem(TodoItemDTO todoDTO)
+        public async Task<ActionResult<TodoItem>> PostTodoItem(TodoItem todo)
         {
-            var todoItem = new TodoItem
-            {
-                IsComplete = todoDTO.IsComplete,
-                Name = todoDTO.Name
-            };
-
-            _context.TodoItems.Add(todoItem);
-            await _context.SaveChangesAsync();
-
-            return CreatedAtAction(nameof(GetTodoItem), new { id = todoItem.Id }, ItemToDTO(todoItem));
+            var created = await _todoService.CreateTodoAsync(todo);
+            return CreatedAtAction(nameof(GetTodoItem), new { id = created.Id }, created);
         }
 
-        // DELETE: api/TodoItems/5
-        [HttpDelete("{id}")]
-        public async Task<IActionResult> DeleteTodoItem(long id)
-        {
-            var todoItem = await _context.TodoItems.FindAsync(id);
-            if (todoItem == null)
-            {
-                return NotFound();
-            }
 
-            _context.TodoItems.Remove(todoItem);
-            await _context.SaveChangesAsync();
+        // private bool TodoItemExists(long id)
+        // {
+        //     return _context.TodoItems.Any(e => e.Id == id);
+        // }
 
-            return NoContent();
-        }
-
-        private bool TodoItemExists(long id)
-        {
-            return _context.TodoItems.Any(e => e.Id == id);
-        }
-
+        // Basically converts the TodoItem model class to the DTO without needing to pass 
+        // in the "Secret" field from the original class. 
         private static TodoItemDTO ItemToDTO(TodoItem todoItem) =>
             new TodoItemDTO
             {
