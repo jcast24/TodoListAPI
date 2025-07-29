bc7cacaa71fe91fe8a29914f6014890e2f6ece56
Jesus Castro <castrojesus39@gmail.com>
2025-07-28 18:17:27 -0700

    user model for auth

---
+++ Models/User.cs
@@ -0,0 +1,8 @@
+namespace TodoApi.Models;
+
+public class User
+{
+    public int Id { get; set; }
+    public string Username { get; set; } = String.Empty;
+    public string PasswordHash { get; set; } = String.Empty;
+}
