211168e26d5473e873fec8b39342710a6eef74c9
Jesus Castro <castrojesus39@gmail.com>
2025-08-26 21:12:48 -0700

    included List of TodoItems as part of the User class

--- Models/User.cs
+++ Models/User.cs
@@ -2,7 +2,12 @@ namespace TodoApi.Models;
 
 public class User
 {
-    public int Id { get; set; }
+    public int Id { get; set; } // Primary key
     public string Username { get; set; } = String.Empty;
-    public string PasswordHash { get; set; } = String.Empty;
+    public string Email { get; set; } = String.Empty;
+    public string PasswordHash { get; set; } = String.Empty; // HashedPassword and not plain text
+
+
+    // Navigation property for related todo items
+    public List<TodoItem> TodoItems { get; set; } = new();
 }
