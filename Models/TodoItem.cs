557146e42e37d378166eb0d9211dab39ce2e2b4d
Jesus Castro <castrojesus39@gmail.com>
2025-08-26 21:10:57 -0700

    added two new fields: UserId and User and changed Name to Title

--- Models/TodoItem.cs
+++ Models/TodoItem.cs
@@ -3,7 +3,13 @@ namespace TodoApi.Models;
 public class TodoItem
 {
     public int Id { get; set; }
-    public string? Name { get; set; }
-    public bool IsComplete { get; set; }
-    public string? Secret { get; set; }
+    public string? Title { get; set; }
+    public bool IsCompleted { get; set; }
+
+    // Foreign Key to user
+    public int UserId { get; set; }
+
+    // Navigation property back to the user
+    public User? User { get; set; }
+
 }
