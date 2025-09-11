fd050630001eee50d21613230203de772183c66d
Jesus Castro <castrojesus39@gmail.com>
2025-09-10 21:22:52 -0700

    completed auth with token + login/register creation

---
+++ Controllers/AuthController.cs
@@ -0,0 +1,83 @@
+using System.Configuration;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.DiaSymReader;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+using TodoApi.Models;
+
+namespace TodoApi.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class AuthController(IConfiguration configuration, TodoContext context) : ControllerBase
+{
+
+    [HttpPost("register")]
+    public async Task<ActionResult<User>> Register(UserDto request)
+    {
+        if (await context.Users.AnyAsync(u => u.Username == request.Username))
+        {
+            return BadRequest("Username already exists");
+        }
+
+        var user = new User();
+        var hashedPassword = new PasswordHasher<User>().HashPassword(user, request.Password);
+        user.Username = request.Username;
+        user.PasswordHash = hashedPassword;
+        context.Users.Add(user);
+        await context.SaveChangesAsync();
+
+        return Ok(user);
+    }
+
+    [HttpPost("login")]
+    public async Task<ActionResult<string>> Login(UserDto request)
+    {
+        var user = await context.Users.FirstOrDefaultAsync(u => u.Username == request.Username);
+
+        if (user is null)
+        {
+            return BadRequest("User not found");
+        }
+
+        if (user.Username != request.Username)
+        {
+            return BadRequest("User not found");
+        }
+
+        if (new PasswordHasher<User>().VerifyHashedPassword(user, user.PasswordHash, request.Password) == PasswordVerificationResult.Failed)
+        {
+            return BadRequest("Wrong password!");
+        }
+
+        string token = CreateToken(user);
+        return Ok(token);
+    }
+
+    private string CreateToken(User user)
+    {
+        var claims = new List<Claim>
+        {
+            new Claim(ClaimTypes.Name, user.Username)
+        };
+
+        var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(configuration.GetValue<string>("AppSettings:Token")!));
+
+        var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha512);
+
+        var tokenDescriptor = new JwtSecurityToken(
+            issuer: configuration.GetValue<string>("AppSettings:Issuer"),
+            audience: configuration.GetValue<string>("AppSettings:Audience"),
+            claims: claims,
+            expires: DateTime.UtcNow.AddDays(1),
+            signingCredentials: creds
+        );
+        return new JwtSecurityTokenHandler().WriteToken(tokenDescriptor);
+    }
+}
