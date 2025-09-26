9e01db69866ac9f1ee7aa50c43f35ebbf6670b9a
Jesus Castro <castrojesus39@gmail.com>
2025-09-25 22:52:31 -0700

    added refresh + expiration time for tokens

--- Models/User.cs
+++ Models/User.cs
@@ -7,6 +7,9 @@ public class User
     public string Email { get; set; } = String.Empty;
     public string PasswordHash { get; set; } = String.Empty; // HashedPassword and not plain text
 
+    // Store refresh token
+    public string? RefreshToken { get; set; }
+    public DateTime? RefreshTokenExpiryTime { get; set; }
 
     // Navigation property for related todo items
     public List<TodoItem> TodoItems { get; set; } = new();
