6e6bf83052dd7b44656c850ca832ca62eef2e747
Jesus Castro <castrojesus39@gmail.com>
2025-10-06 22:03:58 -0700

    model for requesting refresh token with userId

---
+++ Models/RefreshTokenRequestDto.cs
@@ -0,0 +1,9 @@
+﻿namespace TodoApi.Models
+{
+    public class RefreshTokenRequestDto
+    {
+        public int UserId { get; set; }
+        public required string RefreshToken { get; set; }
+
+    }
+}
