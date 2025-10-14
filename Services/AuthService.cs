ad0c6cf98be13a2cbe2d5a38af9038c8a422f725
Jesus Castro <castrojesus39@gmail.com>
2025-10-13 22:42:52 -0700

    newly updated auth service file

--- Services/AuthService.cs
+++ Services/AuthService.cs
@@ -1,22 +1,103 @@
-﻿using TodoApi.Models;
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Security.Cryptography;
+using System.Text;
+using TodoApi.Models;
 
 namespace TodoApi.Services
 {
-    public class AuthService : IAuthService
+    public class AuthService(TodoContext context, IConfiguration configuration) : IAuthService
     {
-        public Task<TokenResponseDto?> LoginAsync(UserDto request)
+        public async Task<TokenResponseDto?> LoginAsync(UserDto request)
         {
-            throw new NotImplementedException();
-        }
+            var user = await context.Users.FirstOrDefaultAsync(u => u.Username == request.Username);
 
+            if (user is null)
+            {
+                return null;
+            }
+
+            if (new PasswordHasher<User>().VerifyHashedPassword(user, user.PasswordHash, request.Password) == PasswordVerificationResult.Failed)
+            {
+                return null;
+            }
+
+            return await CreateTokenResponse(user);
+        }
+        public async Task<User?> RegisterAsync(UserDto request)
+        {
+            if (await context.Users.AnyAsync(u => u.Username == request.Username))
+            {
+                return null;
+            }
+
+            var user = new User();
+            var hashedPassword = new PasswordHasher<User>().HashPassword(user, request.Password);
+
+            user.Username = request.Username;
+            user.PasswordHash = hashedPassword;
+
+            context.Users.Add(user);
+            await context.SaveChangesAsync();
+            return user;
+        }
         public Task<TokenResponseDto?> RefreshTokenAsync(RefreshTokenRequestDto request)
         {
             throw new NotImplementedException();
         }
 
-        public Task<User?> RegisterAsync(UserDto request)
+        private async Task<TokenResponseDto> CreateTokenResponse(User user)
         {
-            throw new NotImplementedException();
+            return new TokenResponseDto
+            {
+                AccessToken = CreateToken(user),
+                RefreshToken = await GenerateAndSaveRefreshTokenAsync(user)
+            };
+        }
+
+        private string CreateToken(User user)
+        {
+            var claims = new List<Claim>
+            {
+                new Claim(ClaimTypes.Name, user.Username),
+                new Claim(ClaimTypes.NameIdentifier, user.Id.ToString()),
+            };
+
+            var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(configuration.GetValue<string>("AppSettings:Token")!));
+
+            var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha512);
+
+            var tokenDescriptor = new JwtSecurityToken(
+                issuer: configuration.GetValue<string>("AppSettings:Issuer"),
+                audience: configuration.GetValue<string>("AppSettings:Audience"),
+                claims: claims,
+                expires: DateTime.UtcNow.AddDays(1),
+                signingCredentials: creds
+                );
+
+            return new JwtSecurityTokenHandler().WriteToken(tokenDescriptor);
+        }
+
+
+        // Generate refresh token
+        private string GenerateRefreshToken()
+        {
+            var randomNumber = new byte[32];
+            using var rng = RandomNumberGenerator.Create();
+            rng.GetBytes(randomNumber);
+            return Convert.ToBase64String(randomNumber);
+        }
+
+        private async Task<string> GenerateAndSaveRefreshTokenAsync(User user)
+        {
+            var refreshToken = GenerateRefreshToken();
+            user.RefreshToken = refreshToken;
+            user.RefreshTokenExpiryTime = DateTime.UtcNow.AddDays(7);
+            await context.SaveChangesAsync();
+            return refreshToken;
         }
     }
 }
