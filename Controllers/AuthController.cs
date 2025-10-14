1c13b2113a0633bfa3f7b2b316e3224256f9e24e
Jesus Castro <castrojesus39@gmail.com>
2025-10-13 22:42:13 -0700

    separated logic to authService and kept http logic in authcontroller

--- Controllers/AuthController.cs
+++ Controllers/AuthController.cs
@@ -1,34 +1,22 @@
-using System.IdentityModel.Tokens.Jwt;
-using System.Security.Claims;
-using System.Security.Cryptography;
-using System.Text;
-using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.EntityFrameworkCore;
-using Microsoft.IdentityModel.Tokens;
 using TodoApi.Models;
+using TodoApi.Services;
 
 namespace TodoApi.Controllers;
 
 [Route("api/[controller]")]
 [ApiController]
-public class AuthController(IConfiguration configuration, TodoContext context) : ControllerBase
+public class AuthController(IAuthService authService) : ControllerBase
 {
 
     [HttpPost("register")]
     public async Task<ActionResult<User>> Register(UserDto request)
     {
-        if (await context.Users.AnyAsync(u => u.Username == request.Username))
+        var user = await authService.RegisterAsync(request);
+        if (user is null)
         {
             return BadRequest("Username already exists");
         }
-
-        var user = new User();
-        var hashedPassword = new PasswordHasher<User>().HashPassword(user, request.Password);
-        user.Username = request.Username;
-        user.PasswordHash = hashedPassword;
-        context.Users.Add(user);
-        await context.SaveChangesAsync();
 
         return Ok(user);
     }
@@ -36,30 +24,11 @@ public class AuthController(IConfiguration configuration, TodoContext context) :
     [HttpPost("login")]
     public async Task<ActionResult<TokenResponseDto>> Login(UserDto request)
     {
-        var user = await context.Users.FirstOrDefaultAsync(u => u.Username == request.Username);
-
-        if (user is null || user.Username != request.Username)
+        var response = await authService.LoginAsync(request);
+        if (response is null)
         {
-            return BadRequest("User not found");
+            return BadRequest("Invalid username or password");
         }
-
-        // if (user.Username != request.Username)
-        // {
-        //     return BadRequest("User not found");
-        // }
-
-        if (new PasswordHasher<User>().VerifyHashedPassword(user, user.PasswordHash, request.Password) == PasswordVerificationResult.Failed)
-        {
-            return BadRequest("Wrong password!");
-        }
-
-        // string token = CreateToken(user);
-        //var response = new TokenResponseDto
-        //{
-        //    AccessToken = CreateToken(user),
-        //    RefreshToken = await GenerateAndSaveRefreshToken(user)
-        //};
-        var response = await CreateTokenResponse(user);
 
         return Ok(response);
     }
@@ -67,7 +36,7 @@ public class AuthController(IConfiguration configuration, TodoContext context) :
     [HttpPost("refresh-token")]
     public async Task<ActionResult<TokenResponseDto>> RefreshToken(RefreshTokenRequestDto request)
     {
-        var result = await RefreshTokensAsync(request);
+        var result = await authService.RefreshTokenAsync(request);
         if (result is null || result.AccessToken is null || result.RefreshToken is null)
         {
             return Unauthorized("Invalid refresh token");
@@ -76,71 +45,4 @@ public class AuthController(IConfiguration configuration, TodoContext context) :
         return Ok(result);
     }
 
-    private string CreateToken(User user)
-    {
-        var claims = new List<Claim>
-        {
-            new Claim(ClaimTypes.Name, user.Username),
-            new Claim(ClaimTypes.NameIdentifier, user.Id.ToString())
-        };
-
-        var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(configuration.GetValue<string>("AppSettings:Token")!));
-
-        var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha512);
-
-        var tokenDescriptor = new JwtSecurityToken(
-            issuer: configuration.GetValue<string>("AppSettings:Issuer"),
-            audience: configuration.GetValue<string>("AppSettings:Audience"),
-            claims: claims,
-            expires: DateTime.UtcNow.AddDays(1),
-            signingCredentials: creds
-        );
-        return new JwtSecurityTokenHandler().WriteToken(tokenDescriptor);
-    }
-
-    private async Task<TokenResponseDto> CreateTokenResponse(User? user)
-    {
-        return new TokenResponseDto
-        {
-            AccessToken = CreateToken(user),
-            RefreshToken = await GenerateAndSaveRefreshToken(user)
-        };
-    }
-
-    public async Task<TokenResponseDto?> RefreshTokensAsync(RefreshTokenRequestDto request)
-    {
-        var user = await ValidateRefreshTokenAsync(request.UserId, request.RefreshToken);
-
-        if (user is null)
-            return null;
-
-        return await CreateTokenResponse(user);
-
-    }
-    
-    private async Task<User?> ValidateRefreshTokenAsync(int userId, string refreshToken)
-    {
-        var user = await context.Users.FindAsync(userId);
-        if (user is null || user.RefreshToken != refreshToken || user.RefreshTokenExpiryTime <= DateTime.UtcNow) return null;
-
-        return user;
-    }
-
-    // generate refresh token 
-    private string GenerateRefreshToken()
-    {
-        var randomNumber = new byte[32];
-        using var rng = RandomNumberGenerator.Create();
-        rng.GetBytes(randomNumber);
-        return Convert.ToBase64String(randomNumber);
-    }
-
-    private async Task<string> GenerateAndSaveRefreshToken(User user)
-    {
-        var refreshToken = GenerateRefreshToken();
-        user.RefreshToken = refreshToken;
-        user.RefreshTokenExpiryTime = DateTime.UtcNow.AddDays(7);
-        await context.SaveChangesAsync();
-        return refreshToken;
-    }
 }
