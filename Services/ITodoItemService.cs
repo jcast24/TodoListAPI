5e42438a637798c6b678c315bd4e8eafbecdcaee
Jesus Castro <castrojesus39@gmail.com>
2025-10-14 21:23:54 -0700

    updated service methods for TodoItemService/Controller

--- Services/ITodoItemService.cs
+++ Services/ITodoItemService.cs
@@ -5,11 +5,11 @@ namespace TodoApi.Services;
 public interface ITodoItemService
 {
     Task<TodoItem?> GetTodoItemAsync(int id);
-    Task<IEnumerable<TodoItem>> GetAllTodoItems();
+    Task<IEnumerable<TodoItem>> GetAllUserTodoItemsAsync(int userId);
 
-    Task<TodoItem> CreateTodoAsync(TodoItem todo);
+    Task<TodoItem> CreateTodoAsync(int userId, TodoItem todo);
 
-    Task<bool> UpdateTodoAsync(TodoItem todo);
+    Task<TodoItem?> UpdateTodoAsync(int userId, int todoId, TodoItem todo);
 
-    Task<bool> DeleteTodoAsync(int id);
+    Task<bool> DeleteTodoAsync(int userId, int todoId);
 }
