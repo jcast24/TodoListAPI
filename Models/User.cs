54e7721579840e95bb850613a458eff0937d32c2
Jesus Castro <castrojesus39@gmail.com>
2025-10-31 21:49:26 -0700

    added role field for class

--- Models/User.cs
+++ Models/User.cs
@@ -3,9 +3,9 @@ namespace TodoApi.Models;
 public class User
 {
     public int Id { get; set; } // Primary key
-    public string Username { get; set; } = String.Empty;
-    public string Email { get; set; } = String.Empty;
-    public string PasswordHash { get; set; } = String.Empty; // HashedPassword and not plain text
+    public string Username { get; set; } = string.Empty;
+    public string Email { get; set; } = string.Empty;
+    public string PasswordHash { get; set; } = string.Empty; // HashedPassword and not plain text
 
     // Store refresh token
     public string? RefreshToken { get; set; }
@@ -13,4 +13,6 @@ public class User
 
     // Navigation property for related todo items
     public List<TodoItem> TodoItems { get; set; } = new();
+
+    public string Role { get; set; } = string.Empty;
 }
